c427296fc543c5930757282fbd81f9d8346ba42a
MiaoJiaoSan <lpfnwndproc@163.com>
2020-02-12 18:46:02 +0800

    ignore

--- immortal/src/main/java/cn/net/immortal/App.java
+++ immortal/src/main/java/cn/net/immortal/App.java
@@ -5,11 +5,9 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args ) {
+public class App {
+    public static void main(String[] args) {
 
         AnnotationConfigApplicationContext context
                 = new AnnotationConfigApplicationContext("cn.net.immortal");
